1fec1316ecbf6a7b5665c663e172b62b245b5b77
rizkih1109 <rizkihidayatulloh1305@gmail.com>
2024-09-17 19:42:00 +0700

    redux crud done

--- lib/redux/phonebooks/phonebooksApi.ts
+++ lib/redux/phonebooks/phonebooksApi.ts
@@ -6,3 +6,10 @@ export const add = (name: string, phone: string) => api.post('phonebooks', {
     name,
     phone
  })
+
+export const remove = (id: string) => api.delete(`phonebooks/${id}`)
+
+export const edit = (id: string, name: string, phone: string) => api.put(`phonebooks/${id}`, {
+    name,
+    phone
+})
--- lib/redux/phonebooks/phonebooksSlice.ts
+++ lib/redux/phonebooks/phonebooksSlice.ts
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import { add, load } from './phonebooksApi'
+import { add, edit, load, remove } from './phonebooksApi'
 
 interface UserState {
   value: User[],
@@ -16,11 +16,27 @@ export const loadPhoneAsync = createAsyncThunk(
   }
 )
 
-export const addPhoneAsync = createAsyncThunk<AddResponse, NewUser>(
+export const addPhoneAsync = createAsyncThunk<FormResponse, NewUser>(
   'phonebooks/add',
   async ({ id, name, phone }) => {
-    const response = await add(name, phone)
-    return { id, user: response.data }
+    const response = await add(name, phone);
+    return { id, user: response.data };
+  }
+);
+
+export const editPhoneAsync = createAsyncThunk<FormResponse, NewUser>(
+  'phonebooks/edit',
+  async ({id, name, phone}) => {
+    const response = await edit(id, name, phone)
+    return {id, user: response.data}
+  }
+)
+
+export const removePhoneAsync = createAsyncThunk(
+  `phonebooks/remove`,
+  async (id: string) => {
+    const response = await remove(id)
+    return response.data
   }
 )
 
@@ -48,12 +64,37 @@ export const phoneSlice = createSlice({
       })
       .addCase(addPhoneAsync.fulfilled, (state, action) => {
         state.status = 'idle'
-        console.log(action.payload.user)
         state.value = [action.payload.user, ...state.value]
       })
-      .addCase(addPhoneAsync.rejected, (state, action) => {
+      .addCase(editPhoneAsync.rejected, (state, action) => {
         state.status = 'idle'
         console.error('Failed to add user:', action.error.message);
+      })
+      .addCase(editPhoneAsync.pending, state => {
+        state.status = 'loading'
+      })
+      .addCase(editPhoneAsync.fulfilled, (state, action) => {
+        state.status = 'idle'
+        state.value = state.value.map((item: User) => {
+          if (item.id === action.payload.id) {
+            console.log(action.payload.user)
+            item.name = action.payload.user.name
+            item.phone = action.payload.user.phone
+          }
+          return item
+        }) 
+      })
+
+      .addCase(removePhoneAsync.pending, state => {
+        state.status = 'loading'
+      })
+      .addCase(removePhoneAsync.fulfilled, (state, action) => {
+        state.status = 'idle'
+        state.value = state.value.filter(item => item.id !== action.payload.id)
+      })
+      .addCase(removePhoneAsync.rejected, (state, action) => {
+        state.status = 'idle'
+        console.error('Failed to remove user:', action.error.message);
       })
   }
 })
